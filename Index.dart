446149c0f14055267177bccd6a61cbe57b009f0b
Walter Mike <waltermike610@gmail.com>
2024-06-12 15:15:36 +0300

    First time trying dart

---
+++ Index.dart
@@ -0,0 +1,10 @@
+void main (){
+  print("Hello World");
+  print("Soft boy programing 8514");
+  //data types
+  //int --->number --> whole numbers
+  int age = 20 ;
+  //doublr ---> number decimal numbers
+  //variables
+
+}
